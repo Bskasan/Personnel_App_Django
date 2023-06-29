dbb4f1a4a00afd3f771cf38979b69fa9814b5983
Bskasan <b.kasan@yandex.com>
2023-06-29 23:20:03 +0300

    patterns ready to run

--- users/urls.py
+++ users/urls.py
@@ -3,3 +3,7 @@ from django.urls import path, include
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
 ]
+
+# An API endpoint is a point at which an API -- the code that allows two software programs to communicate with each other -- connects with the software program. APIs work by sending requests for information from a web application or web server and receiving a response.
+
+# Systems that communicate through APIs are integrated systems. One side sends the information to the API and is called the server. The other side, the client, makes the requests and manipulates the API. The server side that provides the requested information, or resources, is the API endpoint.
