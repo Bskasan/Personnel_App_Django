a7c04efc7da2abc2ab2338c2c47784e2b8152942
Bskasan <b.kasan@yandex.com>
2023-07-02 01:51:29 +0300

    new logout method done and created IsOwnerOrStar permission

--- users/views.py
+++ users/views.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from .models import Profile
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrStaff
 
 # Create your views here.
 class RegisterView(CreateAPIView):
@@ -31,3 +34,12 @@ class RegisterView(CreateAPIView):
 class ProfileUpdateView(RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrStaff)
+
+@api_view(['POST'])
+def logout(request):
+    request.user.auth_token.delete()
+    data = {
+        'message':'Logged out successfully!'
+    }
+    return Response(data, status=status.HTTP_200_OK)
