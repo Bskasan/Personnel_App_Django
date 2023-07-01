6a3c68cf8281a0f69b1a56ca778a6beccb6ad6fc
Bskasan <b.kasan@yandex.com>
2023-07-02 01:17:54 +0300

    create token method

---
+++ users/signals.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from .models import Profile
+
+# Creating Tokens for signed users.
+@receiver(post_save, sender=User)
+def create_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
